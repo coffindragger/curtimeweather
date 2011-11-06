8333e62ba872cf5c475c68294fb6e89fc94e5a42
M. Wiggins <code@hlidskialf.com>
2011-11-06 15:15:28 -0800

    sun/moon rise/set

--- jquery.curweather.js
+++ jquery.curweather.js
@@ -16,6 +16,18 @@
             callback(cache);
         }
     }
+    function cached_post(key, url, url_data, callback) {
+        var cache = JSON.parse(localStorage.getItem(key));
+        if (!cache) {
+            $.post(url, url_data, function(data) {
+                localStorage.setItem(key, JSON.stringify(data));
+                callback(data);
+            });
+        }
+        else {
+            callback(cache);
+        }
+    }
 
 
 
@@ -33,19 +45,73 @@
                 'day': d,
             };
 
+
+
+            /* wunderground moon phase*/
             var key = "astronomy_"+data.options.ZIPCODE+d.getYear()+d.getMonth()+d.getDay();
             var url = "http://api.wunderground.com/api/"+data.options.WUNDERGROUND_KEY+"/astronomy/q/"+data.options.ZIPCODE+".json";
             cached_get(key, url, function(obj) {
-                data.latest_day.sunrise = new Date();
-                data.latest_day.sunrise.setHours(obj.moon_phase.sunrise.hour);
-                data.latest_day.sunrise.setMinutes(obj.moon_phase.sunrise.minute);
-                data.sunrise.html(data.latest_day.sunrise.getHours()+":"+data.latest_day.sunrise.getMinutes());
+                data.latest_day.moon_illum = obj.moon_phase.percentIlluminated
+                data.latest_day.moon_age = obj.moon_phase.ageOfMoon
 
-                data.latest_day.sunset = new Date();
-                data.latest_day.sunset.setHours(obj.moon_phase.sunset.hour);
-                data.latest_day.sunset.setMinutes(obj.moon_phase.sunset.minute);
-                data.sunset.html(data.latest_day.sunset.getHours()+":"+data.latest_day.sunset.getMinutes());
+                var wax_or_wane = 'wax'
+                if (data.latest_day.moon_age >= 15) {
+                    wax_or_wane = 'wane'
+                }
+
+                data.moon.html('<img src="img/moonvis/moon_'+wax_or_wane+'_'+data.latest_day.moon_illum+'.jpg"/>')
             });
+
+
+            // us navy {sun,moon}{rise,set} times
+            var key = "usno.navy.mil_"+data.options.ZIPCODE+d.getYear()+d.getMonth()+d.getDay();
+            var url = "http://aa.usno.navy.mil/cgi-bin/aa_pap.pl"
+            var url_data = {
+                'FFX': 1,
+                'ID': 'CSKY',
+                'xxy': d.getFullYear(),
+                'xxm': d.getMonth()+1,
+                'xxd': d.getDate(),
+                'st': 'OR',
+                'place': 'eugene',
+                'ZZZ': 'END',
+            };
+            cached_post(key, url, url_data, function(response) {
+
+                var ampm2date = function(s) {
+                    var d = new Date()
+                    var parts = s.split(/[: ]/)
+                    var h = parts[0]
+                    var m = parts[1]
+                    if (parts[2].match(/p.m./)) {
+                        h = 12+Number(h);
+                    }
+                    d.setHours(h);
+                    d.setMinutes(m);
+                    return d;
+
+                }
+
+                //var $pre = $(response);//.find('pre');
+                var resp = String(response);
+
+                //var re = new RegExp("", 'i')
+                if ((matches = resp.match(/Sunrise\s+(\d+:\d+ [ap]\.m\.)/)))
+                    data.latest_day.sunrise = ampm2date(matches[1])
+                if ((matches = resp.match(/Sunset\s+(\d+:\d+ [ap]\.m\.)/)))
+                    data.latest_day.sunset = ampm2date(matches[1])
+                if ((matches = resp.match(/Moonrise\s+(\d+:\d+ [ap]\.m\.)  /)))
+                    data.latest_day.moonrise = ampm2date(matches[1])
+                if ((matches = resp.match(/Moonset\s+(\d+:\d+ [ap]\.m\.) on following day/)))
+                    data.latest_day.moonset = new Date(ampm2date(matches[1]).getTime() + 86400000);
+
+                data.sunset.html(data.latest_day.sunset.getHours()+":"+data.latest_day.sunset.getMinutes());
+                data.sunrise.html(data.latest_day.sunrise.getHours()+":"+data.latest_day.sunrise.getMinutes());
+                data.moonset.html(data.latest_day.moonset.getHours()+":"+data.latest_day.moonset.getMinutes());
+                data.moonrise.html(data.latest_day.moonrise.getHours()+":"+data.latest_day.moonrise.getMinutes());
+
+            });
+
         }
 
 
@@ -53,30 +119,27 @@
         data.time.html(s);
 
 
-        // update sun/moon
-        data.sun.html('<img src="img/sun.jpg"/>')
-        var rise = data.latest_day.sunrise.getTime();
-        var set = data.latest_day.sunset.getTime();
-        var now = d.getTime();
-        var range = set - rise;
-        var cur = (now - rise) / range;
+        function animate_rising_setting(origin_x, origin_y, radius, target, rise, set)
+        {
+            var now = new Date().getTime();
+            var range = set - rise;
+            var cur = (now - rise) / range;
+            var radians = (2*Math.PI) - (cur * Math.PI) - (0.5*Math.PI);
+            if (now > set) { // night time
+                target.addClass('night')
+            }
+            else {
+                target.removeClass('night')
+            }
 
-        var radians = (2*Math.PI) - (cur * Math.PI) - (0.5*Math.PI);
-        if (now > set) { // night time
-            radians += Math.PI;  // add a half circle
-            data.sun.addClass('night')
+            var x = Math.round(origin_x + Math.sin(radians) * radius - 32);
+            var y = Math.round(origin_y + Math.cos(radians) * radius - 32);
+            if (x != target.x || y != target.y) {
+                target.animate({'left': x+"px", 'top': y+"px"}, 500)
+            }
         }
-        else {
-            data.sun.removeClass('night')
-        }
-
-        //var cur = d.getSeconds() / 59;
-
-        var r = 100;
-        var x = 150 + Math.sin(radians) * r - 32;
-        var y = 150 + Math.cos(radians) * r - 32;
-        data.sun.animate({'left': x+"px", 'top': y+"px"}, 500)
-
+        animate_rising_setting(150, 150, 100, data.sun, data.latest_day.sunrise.getTime(), data.latest_day.sunset.getTime());
+        animate_rising_setting(150, 150, 100, data.moon, data.latest_day.moonrise.getTime(), data.latest_day.moonset.getTime());
 
         // re-tick
         setTimeout(tick, 500, data);
@@ -103,16 +166,18 @@
                     $this.data(PLUGIN, data);
 
 
-                    data.sun = $('<div class="sun"/>')
-                    $this.append(data.sun)
-                    data.moon = $('<div class="moon"/>')
-                    $this.append(data.moon)
-                    data.sunrise = $('<div class="sunrise"/>')
-                    $this.append(data.sunrise)
-                    data.time = $('<div class="time"/>')
-                    $this.append(data.time)
-                    data.sunset = $('<div class="sunset"/>')
-                    $this.append(data.sunset)
+
+                    var $container = $('<div>  <div class="sun"><img src="img/sun.jpg"/></div>  <div class="moon"></div>  <div class="time"/><div class="rise"><div class="sunrise"/><div class="moonrise"/></div><div class="set"><div class="sunset"/><div class="moonset"/></div> </div>')
+                    $this.append($container)
+
+                    data.sun = $container.find('.sun')
+                    data.moon = $container.find('.moon')
+                    data.sunrise = $container.find('.sunrise')
+                    data.sunset = $container.find('.sunset')
+                    data.moonrise = $container.find('.moonrise')
+                    data.moonset = $container.find('.moonset')
+                    data.time = $container.find('.time')
+
 
                     tick(data);
 
