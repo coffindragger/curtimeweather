91be73480de2d46118f23050425dba07cdf589aa
M. Wiggins <code@hlidskialf.com>
2011-11-06 18:12:51 -0800

    history tick

--- jquery.curweather.js
+++ jquery.curweather.js
@@ -42,7 +42,6 @@
     function tick(data) {
         d = new Date();
 
-        
         // update clock
         data.time.html(format_time(d));
 
@@ -50,7 +49,6 @@
             data.latest_day = {
                 'day': d,
             };
-
 
 
             /* wunderground moon phase*/
@@ -68,6 +66,19 @@
                 data.moon.html('<img src="img/moonvis/moon_'+wax_or_wane+'_'+data.latest_day.moon_illum+'.jpg"/>')
             });
 
+            var key = "history_"+data.options.ZIPCODE+d.getYear()+d.getMonth()+d.getDay()+d.getHours();
+            var url = "http://api.wunderground.com/api/"+data.options.WUNDERGROUND_KEY+"/history/q/"+data.options.ZIPCODE+".json";
+            cached_get(key, url, function(obj) {
+                for (var i=0; i<obj.history.observations.length; i++) {
+                    var observation = obj.history.observations[i];
+
+                    if (!(observation.metar in data.metar_history)) {
+                        data.metar_history[observation.metar] = true;
+                        data.target.curweather('update', observation.metar);
+                    }
+
+                }
+            });
 
             // us navy {sun,moon}{rise,set} times
             var key = "usno.navy.mil_"+data.options.ZIPCODE+d.getYear()+d.getMonth()+d.getDay();
@@ -167,6 +178,7 @@
                         options: options,
 
                         lastest_day: null,
+                        metar_history: {},
                     };
                     $this.data(PLUGIN, data);
 
@@ -208,8 +220,6 @@
                 data = $this.data(PLUGIN);
 
                 data.metar = parse_metar(metar_string)
-                console.log(data.metar)
-
 
                 data.temperature.html(Math.round(data.metar.temp_f))
                 data.dewpoint.html(Math.round(data.metar.dewpoint_f))
@@ -244,6 +254,154 @@
             $.error('Method '+method+'does not exist on jQuery.'+PLUGIN);
         }
     }
+
+
+    function parse_metar(metar_string) {
+        var metar = {
+            clouds: [],
+            weather: [],
+        }
+
+        var parts = metar_string.split(/\s+/)
+        metar.type = parts.shift(); // METAR or SPECI
+        metar.loc = parts.shift(); // K***
+        for (var i=0; i < parts.length; i++) {
+            var chunk = parts[i];
+
+            if (chunk.match(/Z$/)) {
+                // timestamp
+                metar.observe_date = new Date();
+                metar.observe_date.setUTCDate(chunk.slice(0,2))
+                metar.observe_date.setUTCHours(chunk.slice(2,4))
+                metar.observe_date.setUTCMinutes(chunk.slice(4,6))
+                metar.observed_at = metar.observe_date.toString()
+            }
+            else
+            if (chunk.match(/KT$/)) {
+                // wind direction and speed
+                metar.wind_dir = Number(chunk.slice(0,3))
+                metar.wind_spd = Number(chunk.slice(3,5))
+                if (chunk[5] == 'G') {
+                    metar.wind_gusts = Number(chunk.slice(5).replace(/KT$/,''))
+                }
+            }
+            else
+            if ((matches = chunk.match(/(\d+)(\/(\d+))?SM$/))) {
+                //visibility
+                if (matches[2]) {
+                    metar.visibility_mi = Number(matches[1]) / Number(matches[3]);
+                }
+                else {
+                    metar.visibility_mi = 1;
+                }
+            }
+            else
+            if (chunk.match(/^OVC/) || chunk.match(/^BKN/) || chunk.match(/^SCT/) || chunk.match(/^FEW/) || chunk.match(/^SKC/)) {
+                //cloud coverage
+                metar.clouds.push( [chunk.slice(0,3), Number(chunk.slice(3,6))*100] )
+            }
+            else
+            if ((matches = chunk.match(/(M?)(\d+)\/(M?)(\d+)/))) {
+                // TEMP/DEW
+                metar.temp_c = Number(matches[2])
+                if (matches[1] == 'M') {
+                    metar.temp_c *= -1;
+                }
+                metar.dewpoint_c = Number(matches[4])
+                if (matches[3] == 'M') {
+                    metar.dewpoint_c *= -1;
+                }
+            }
+            else
+            if (chunk.match(/^T/)) {
+                // RMK T* temperature/dewpoint
+                metar.temp_c = Number(chunk.slice(2,5))/10
+                if (chunk[1] == '1') {
+                    metar.temp_c *= -1;
+                }
+                metar.dewpoint_c = Number(chunk.slice(6,9))/10
+                if (chunk[5] == '1') {
+                    metar.dewpoint_c *= -1;
+                }
+            }
+            else
+            if (chunk.match(/^AO2/)) {
+            }
+            else
+            if (chunk.match(/^A/)) {
+                // altimeter pressure
+                metar.pressure_in = Number(chunk.slice(1))/100;
+            }
+            else
+            if (chunk.match(/^SLP/)) {
+                // RMK SLP* sea level pressure
+                metar.pressure_sea = Number("10"+chunk.slice(3))/10;
+            }
+            else
+            if ((matches = chunk.match(/([-+]?)(VC)?(BC|BL|DR|FZ|MI|PR|SH|TS)?(DZ|GR|GS|IC|PL|RA|SG|SN|UP)?(BR|DU|FG|FU|HZ|PY|SA|VA)?(DS|FC|PO|SQ|SS)?/))) {
+                var abbr = {
+                    'BC': "Patchy",
+                    'BL': "Blowing",
+                    'DR': "Low Drifting",
+                    'FZ': "Freezing",
+                    'MI': "Shallow",
+                    'PR': "Partial",
+                    'SH': "Shower",
+                    'TS': "Thunderstorm",
+                    'DZ': "Drizzle",
+                    'GR': "Hail",
+                    'GS': "Hail+Snow",
+                    'IC': "Ice Crystals",
+                    'PL': "Ice Pellets",
+                    'RA': "Rain",
+                    'SG': "Snow Grains",
+                    'SN': "Snow",
+                    'BR': "Mist",
+                    'DU': "Widespread Dust",
+                    'FG': "Fog",
+                    'FU': "Smoke",
+                    'HZ': "Haze",
+                    'PY': "Spray",
+                    'SA': "Sand",
+                    'VA': "Volcanic Ash",
+                    'DS': "Dust Storm",
+                    'FC': "Funnel Cloud",
+                    'PO': "Dust/Sand Whirls",
+                    'SQ': "Squalls",
+                    'SS': "Sandstorm",
+                };
+
+                var out = []
+                if (matches[1] == '-') 
+                    out.push('Light')
+                else
+                if (matches[1] == '+')
+                    out.push('Heavy')
+
+                out.push(abbr[matches[3]])
+                out.push(abbr[matches[4]])
+                out.push(abbr[matches[5]])
+
+                if (matches[2] == 'VC') 
+                    out.push("In Vicinity")
+
+                var s = out.join(" ").replace(/\s+/, ' ').trim()
+                if (s) {
+                    metar.weather.push(s)
+                }
+            }
+
+        }
+
+        metar.clouds.sort(function (a,b) {
+            return a[1] - b[1];
+        })
+        metar.temp_f = metar.temp_c * (9/5) + 32;
+        metar.dewpoint_f = metar.dewpoint_c * (9/5) + 32;
+
+        return metar;
+    }
+
 
 })(jQuery);
 
